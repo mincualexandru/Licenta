7bc67b20b7396ac73522f90e4a967ab78975f834
mincualexandru <mincualexandru1998@gmail.com>
2020-03-20 20:32:30 +0200

    remove files.

--- web/src/main/java/com/web/dao/IExerciseImageDao.java
+++
@@ -1,9 +0,0 @@
-package com.web.dao;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.web.model.ExerciseImage;
-
-public interface IExerciseImageDao {
-	void saveImage(MultipartFile imageFile, ExerciseImage exerciseImage) throws Exception;
-}
